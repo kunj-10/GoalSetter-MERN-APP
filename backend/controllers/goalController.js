5a887badb3fbaf45c4278d8775dc1ab8c836e3f6
kunj_10 <kunjvgoyal30@gmail.com>
2024-12-17 13:56:33 +0530

    Added errorHandler middleware

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,28 +1,35 @@
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req, res) => {
+const getGoals = async (req, res) => {
     res.status(200).json({message: 'Get Goals'})
 }
 
 // @desc    Set goals
 // @route   POST /api/goals
 // @access  Private
-const setGoal = (req, res) => {
+const setGoal = async (req, res) => {
+    
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+        return
+    }
+
     res.status(200).json({message: 'Set goal'})
 }
 
 // @desc    Update goals
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateGoal = (req, res) => {
+const updateGoal = async (req, res) => {
     res.status(200).json({message: `Update goal ${req.params.id}`})
 }
 
 // @desc    Delete goals
 // @route   delete /api/goals/:id
 // @access  Private
-const deleteGoal = (req, res) => {
+const deleteGoal = async (req, res) => {
     res.status(200).json({message: `Delete goal ${req.params.id}`})
 }
 
